0302d6bd38f84d3a75c676bf142b581466b7436f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 00:57:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
     elsif array.size == 2 
     array.join(" and ")
     
-    elsif array.size > 3
-    array.join(" , ")
+    elsif array.size >= 3
+    array.join(", ")
     
     
   end
