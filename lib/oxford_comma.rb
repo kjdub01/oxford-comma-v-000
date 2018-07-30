f6c499787ca2f78485c02d9421195719c9d9246f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 00:16:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
-  if array == 1 array.join
-  elsif array == 2 array.join("and")
+  array.join(",")
+  #elsif array == 2 array.join("and")
+  #elsif array > 3 array.join("," && "and" before last index)
   end
 end
