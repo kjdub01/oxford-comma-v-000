67b8290dc0cba90db86eeb3e03be91036c9fce90
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 00:17:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   array.join(",")
+ end 
+  
+  
+  
+  
   #elsif array == 2 array.join("and")
   #elsif array > 3 array.join("," && "and" before last index)
-  end
-end
+  
